f24954eabf51b8c24ac247f2eb49d3faccde698f
MridulRastogi <mridulrastogi100@gmail.com>
2021-07-30 16:01:52 +0530

    learning stl

---
+++ C++/Learning STL/array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<array>
+using namespace std;
+int main()
+{
+    int i;
+    array<int,4> a = {8,5,2,7};
+    for( i=0 ; i<a.size() ; i++ )
+        cout<<a[i]<<" ";
+    cout << endl;
+    cout << "Element at 2nd index: " << a.at(2)   << endl;
+    cout << "Array empty or not: "   << a.empty() << endl;
+    cout << "First element: "        << a.front() << endl;
+    cout << "Last element: "         << a.back()  << endl;
+
+    return 0;
+}
