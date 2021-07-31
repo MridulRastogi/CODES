fc4077ff9c9e8f1a0837e3732d015d4b60346721
MridulRastogi <mridulrastogi100@gmail.com>
2021-07-31 11:50:30 +0530

    Learning STL

--- C++/Learning STL/array.cpp
+++ C++/Learning STL/array.cpp
@@ -8,10 +8,11 @@ int main()
     for( i=0 ; i<a.size() ; i++ )
         cout<<a[i]<<" ";
     cout << endl;
+    
     cout << "Element at 2nd index: " << a.at(2)   << endl;
     cout << "Array empty or not: "   << a.empty() << endl;
     cout << "First element: "        << a.front() << endl;
     cout << "Last element: "         << a.back()  << endl;
-    
+
     return 0;
 }
