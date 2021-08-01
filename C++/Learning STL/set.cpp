858df8a90776de10a229dc9bd7cc0ca6a6948c53
MridulRastogi <mridulrastogi100@gmail.com>
2021-08-01 17:20:16 +0530

    Learning STL

--- C++/Learning STL/set.cpp
+++ C++/Learning STL/set.cpp
@@ -17,13 +17,12 @@ int main()
     s.insert(3);
     s.insert(5);
     s.insert(5);
-    s.insert(2);
-
+// 5 1 6 5 3 5 => 5 1 6 3 => 1 3 5 6
     print(s);
     set<int>::iterator it=s.begin();
     it++;
     s.erase(it);
     print(s);
-
+    cout<<"4 is present or not => "<<s.count(4)<<endl;
     return 0;
 }
