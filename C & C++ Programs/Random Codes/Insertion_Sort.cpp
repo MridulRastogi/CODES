d5bc53e086d95f177c419e0d1b208f9fd78078c1
MridulRastogi <mridulrastogi100@gmail.com>
2021-08-14 01:10:11 +0530

    Changes made in Sorting Algos

--- C & C++ Programs/Random Codes/Insertion_Sort.cpp
+++ C & C++ Programs/Random Codes/Insertion_Sort.cpp
@@ -1,17 +1,26 @@
 #include<iostream>
 using namespace std;
+void print(int a[], int n)
+{
+    for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+}
 int main()
 {
-    int n,i,j,key;
-    cout<<"Enter the size of the array : ";
-    cin>>n;
-    int a[n];
-    cout<<"Enter the elements : ";
-    for(i=0;i<n;i++)
-        cin>>a[i];
-
+    int n=6,i,j,key,comp=0,shifts=0;
+    //cout<<"Enter the size of the array : ";
+    //cin>>n;
+    int a[n]={5,6,3,8,9,7};
+    print(a,n);
+    cout<<endl;
+    //cout<<"Enter the elements : ";
+//    for(i=0;i<n;i++)
+//        cin>>a[i];
+//INSERTION SORT BEGINS
     for(i=1;i<n;i++)
     {
+        cout<<"I = "<<i<<endl;
         key = a[i];
         j = i-1;
         while(j>=0 && key<a[j])
@@ -20,8 +29,11 @@ int main()
             j = j-1;
         }
         a[j+1] = key;
+        print(a,n);
+        cout<<endl;
     }
-
+//INSERTION SORT ENDS
+    cout<<"Sorted Array: ";
     for(i=0;i<n;i++)
         cout<<a[i]<<" ";
     return 0;
