cb0ae39ead2e1b919a163c63ec7ad50f2840662b
MridulRastogi <mridulrastogi100@gmail.com>
2021-12-13 01:18:04 +0530

    DAA Files Added

---
+++ C++/5th Semester/DAA_PRACTICALS/Activity_Selection.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, start, end;
+    cout<<"Enter the number of activities: ";
+    cin>>n;
+    vector<pair<int,int>> a, res;
+    cout<<"Enter the starting and the ending time of the activities:"<<endl;
+    for(int i=0; i<n; i++)
+    {
+        cin>>start>>end;
+        a.push_back(make_pair(end,start));
+    }
+    // SORT IN INCREASING ORDER OF STARTING TIME
+    sort(a.begin(), a.end());
+    cout<<endl;
+    end   = a[0].first;
+    start = a[0].second;
+    res.push_back(make_pair(start,end));
+    for(int i=1; i<n; i++)
+        if(a[i].second > end)
+        {
+            end = a[i].first;
+            res.push_back(make_pair(a[i].second, a[i].first));
+        }
+    for(auto j:res)
+        cout<<j.first<<" "<<j.second<<endl;
+    return 0;
+}
+
+/*
+5
+6 11
+5 7
+1 4
+8 12
+3 6
+*/
