a2c27eb9865a94fd0ef945b260311cc840c4ce89
MridulRastogi <mridulrastogi100@gmail.com>
2022-08-11 11:15:13 +0530

    New Files Added

--- C++/5th Semester/DAA_PRACTICALS/BFS.cpp
+++ C++/5th Semester/DAA_PRACTICALS/BFS.cpp
@@ -9,24 +9,24 @@ int main()
 {
     int i,n,m,x,y,node;
     for(i=0; i<N; i++)
-        vis[i] = 0;         //declaring bool array vis equal to 0
-    cin >> n >> m;          //inputting number of nodes and edges
+        vis[i] = 0;                     //declaring bool array vis equal to 0
+    cin >> n >> m;                      //inputting number of nodes and edges
     for(i=0; i<m; i++)
     {
-        cin>>x>>y;              //inputting edges from x edge to y edge
-        adj[x].push_back(y);
-        adj[y].push_back(x);
+        cin >> x >> y;                  //inputting edges from x edge to y edge
+        adj[x].push_back(y);            //pushing the nodes x from the connection of nodes x and y
+        adj[y].push_back(x);            //pushing the nodes y from the connection of nodes x and y
     }
-    queue<int> q;
-    q.push(1);
-    vis[1] = true;
+    queue<int> q;                       //declaring a queue for inertion from one side and deletion from another side
+    q.push(1);                              //first element as 1 for queue
+    vis[1] = true;                          //first position except 0 allocated with "true"
 
-    while(!q.empty())
+    while(q.empty() == false)           //if queue is not empty (since it already has a one)
     {
-        node = q.front();
-        q.pop();
+        node = q.front();               //getting the first element of queue in an integer variable node
+        q.pop();                        //popping the element from queue extracted in node
         cout<<node<<endl;
-        vector<int> :: iterator it;
+        vector<int> :: iterator it;     //declaring an interator for traversing the adjacency list Vector
         for(it=adj[node].begin(); it!=adj[node].end(); it++)
         {
             if(!vis[*it])
